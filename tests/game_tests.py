102f645f702352862fb2ecc1bc44049b44b01efa
Matt Logan <mlogan5212@gmail.com>
2021-01-27 18:32:23 -0600

    delete, get tests successful

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -58,7 +58,6 @@ class GameTests(APITestCase):
         game.gamer_id = 1
         game.save()
 
-        # DEFINE NEW PROPERTIES FOR GAME
         data = {
             "gameTypeId": 1,
             "skillLevel": 2,
@@ -70,14 +69,48 @@ class GameTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
         response = self.client.put(f"/games/{game.id}", data, format="json")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-        # GET GAME AGAIN TO VERIFY CHANGES
         response = self.client.get(f"/games/{game.id}")
         json_response = json.loads(response.content)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # Assert that the properties are correct
         self.assertEqual(json_response["title"], "Sorry")
         self.assertEqual(json_response["maker"], "Hasbro")
         self.assertEqual(json_response["skill_level"], 2)
         self.assertEqual(json_response["number_of_players"], 4)
+        
+
+    def test_get_game(self):
+        game = Game()
+        game.gametype_id = 1
+        game.skill_level = 5
+        game.title = "Monopoly"
+        game.maker = "Milton Bradley"
+        game.number_of_players = 4
+        game.gamer_id = 1
+
+        game.save()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get(f"/games/{game.id}")
+        json_response = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json_response["title"], "Monopoly")
+        self.assertEqual(json_response["maker"], "Milton Bradley")
+        self.assertEqual(json_response["skill_level"], 5)
+        self.assertEqual(json_response["number_of_players"], 4)
+
+
+    def test_delete_game(self):
+        game = Game()
+        game.gametype_id = 1
+        game.skill_level = 5
+        game.title = "Sorry"
+        game.maker = "Milton Bradley"
+        game.number_of_players = 4
+        game.gamer_id = 1
+        game.save()
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(f"/games/{game.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get(f"/games/{game.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
